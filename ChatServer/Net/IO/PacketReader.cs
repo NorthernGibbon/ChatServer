d562a79e72c113230317a2ffa45032cf13200773
Jacob Davidsson <Jacob.Davidsson@outlook.com>
2025-04-10 18:42:41 +0200

    The complete code for backend chat

---
+++ ChatServer/Net/IO/PacketReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer.Net.IO
+{
+    public class PacketReader : BinaryReader
+    {
+
+        private NetworkStream _ns;
+        public PacketReader(NetworkStream ns) : base(ns) 
+        {
+            _ns = ns;
+        }
+
+        public string ReadMessage()
+        {
+            byte[] msgByuffer;
+            var length = ReadInt32();
+            msgByuffer = new byte[length];
+            _ns.Read(msgByuffer, 0, length);
+
+            var msg = Encoding.ASCII.GetString(msgByuffer);
+
+            return msg;
+        }
+    }
+}
